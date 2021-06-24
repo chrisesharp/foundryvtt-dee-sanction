12a16fa29a391b083e83c99a9c15dc1e022eb9bd
C. E. Sharp <chrisesharp@gmail.com>
2021-06-24 08:47:56 +0100

    v0.2.9

--- src/module/actor/actor-sheet.js
+++ src/module/actor/actor-sheet.js
@@ -238,23 +238,23 @@ export class DeeSanctionActorSheet extends ActorSheet {
    */
   _onRoll(event) {
     event.preventDefault();
-    console.log("here")
     const element = event.currentTarget;
     const resource = element.dataset.resource;
     const dataset = element.dataset;
     let target;
     for (let t of game.user.targets.values()) {
       const data = t.actor.data;
-      target = {
-        id: data._id,
-        armour: data.data.resources.armour.value,
-      }
       if (data.type === "enemy") {
+        target = {
+          id: data._id,
+          armour: data.data.resources.armour.value,
+        }
+      
         target.potency = data.data.resistance.potency;
         target.hitresolution = data.data.hitresolution;
         target.consequences = data.data.consequences;
       }
-      break;
+      if (target) break;
     }
 
     if (dataset.roll) {
